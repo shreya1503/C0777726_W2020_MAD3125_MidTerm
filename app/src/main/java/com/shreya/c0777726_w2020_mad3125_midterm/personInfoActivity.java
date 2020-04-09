34fd9fb72f311ca85ed16b7fff905fecee8eb148
SHREYA <shreyavaghasia99@gmail.com>
2020-04-09 15:30:15 -0400

    [SV] Data Types Added In Person Info Activity

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_midterm/personInfoActivity.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_midterm/personInfoActivity.java
@@ -2,15 +2,57 @@ package com.shreya.c0777726_w2020_mad3125_midterm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Objects;
+import android.widget.DatePicker;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class personInfoActivity extends AppCompatActivity
 {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+    private Button btnCalculate;
+    private EditText edtFirstName;
+    private EditText edtLastName;
+    private EditText edtSinNumber;
+    private EditText edtDOB;
+    private TextView edtTaxFiledDate;
+    private EditText edtGrossIncome;
+    private EditText edtRRSP;
+    private RadioButton rbtnMale;
+    private RadioButton rbtnFemale;
+    private RadioButton rbtnOther;
+    private RadioGroup rdGender;
+
+    private   DatePickerDialog picker;
+    final Calendar calendar = Calendar.getInstance();
+    final int day = calendar.get(Calendar.DAY_OF_MONTH);
+    final   int month = calendar.get(Calendar.MONTH);
+    final int year = calendar.get(Calendar.YEAR);
+    String gender=null;
+
 }
+
+
