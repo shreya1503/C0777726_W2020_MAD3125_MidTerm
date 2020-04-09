b9ea597a2dca21398028e54aa724139c40ac3836
SHREYA <shreyavaghasia99@gmail.com>
2020-04-09 15:07:06 -0400

    [SV] Getter And Setter Added To Data Types in CRA Customer Class

--- app/src/main/java/com/shreya/c0777726_w2020_mad3125_midterm/CRACustomer.java
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_midterm/CRACustomer.java
@@ -23,4 +23,136 @@ public class CRACustomer implements Parcelable
     private double totalTaxableAmount;
     private double totalTaxPaid;
 
+
+    public CRACustomer(String sinNumber, String firstName, String lastName, String personGender, double grossIncome, double rrspContribution)
+    {
+        this.sinNumber = sinNumber;
+        this.firstName = firstName;
+        this.personGender = personGender;
+        this.grossIncome = grossIncome;
+        this.rrspContribution = rrspContribution;
+
+    }
+
+    public String getSinNumber() {
+        return sinNumber;
+    }
+
+    public void setSinNumber(String sinNumber) {
+        this.sinNumber = sinNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFull_name() {
+        return getFirstName()+getLastName();
+    }
+
+    public void setFull_name(String full_name) {
+        this.fullName = full_name;
+    }
+
+    public String getPersonGender() {
+        return personGender;
+    }
+
+    public void setPersonGender(String personGender) {
+        this.personGender = personGender;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Date getFilingTaxDate() {
+        return filingTaxDate;
+    }
+
+    public void setFilingTaxDate(Date filingTaxDate) {
+        this.filingTaxDate = filingTaxDate;
+    }
+
+    public double getFedTax() {
+        return fedTax;
+    }
+
+    public void setFedTax(double fedTax) {
+        this.fedTax = fedTax;
+    }
+
+    public double getProvTax() {
+        return provTax;
+    }
+
+    public void setProvTax(double provTax) {
+        this.provTax = provTax;
+    }
+
+    public double getRrspCarryForward() {
+        return rrspCarryForward;
+    }
+
+    public void setRrspCarryForward(double rrspCarryForward) {
+        this.rrspCarryForward = rrspCarryForward;
+    }
+
+    public double getGrossIncome() {
+        return grossIncome;
+    }
+
+    public void setGrossIncome(double grossIncome) {
+        this.grossIncome = grossIncome;
+    }
+
+    public double getRrspContribution() {
+        return rrspContribution;
+    }
+
+    public void setRrspContribution(double rrspContribution) {
+        this.rrspContribution = rrspContribution;
+    }
+
+    public double getTotalEI() {
+        return totalEI;
+    }
+
+    public void setTotalEI(double totalEI) {
+        this.totalEI = totalEI;
+    }
+
+    public double getTotalTaxableAmount() {
+        return totalTaxableAmount;
+    }
+
+    public void setTotalTaxableAmount(double totalTaxableAmount) {
+        this.totalTaxableAmount = totalTaxableAmount;
+    }
+
+    public double getTotalTaxPaid() {
+        return totalTaxPaid;
+    }
+
+    public void setTotalTaxPaid(double totalTaxPaid) {
+        this.totalTaxPaid = totalTaxPaid;
+    }
+
+
 }
