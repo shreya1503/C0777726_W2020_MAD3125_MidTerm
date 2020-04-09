146b6c99239aaeaaa0a378b2f5c02f9691ba006d
SHREYA <shreyavaghasia99@gmail.com>
2020-04-09 00:05:56 -0400

    [SV] Two New Class File Added

---
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_midterm/dataDisplayActivity.java
@@ -0,0 +1,9 @@
+package com.shreya.c0777726_w2020_mad3125_midterm;
+
+import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class dataDisplayActivity extends AppCompatActivity
+{
+}
---
+++ app/src/main/java/com/shreya/c0777726_w2020_mad3125_midterm/personInfoActivity.java
@@ -0,0 +1,10 @@
+package com.shreya.c0777726_w2020_mad3125_midterm;
+
+import android.app.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class personInfoActivity extends AppCompatActivity
+{
+
+}
